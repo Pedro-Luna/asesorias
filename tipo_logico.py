a6f67a2408fc771e9ccd9caf8ec69974a80d5107
Pedro-Luna <59538557+Pedro-Luna@users.noreply.github.com>
2020-05-18 23:51:04 -0600

    Add files via upload

---
+++ tipo_logico.py
@@ -0,0 +1,40 @@
+valor = 1 + 1 == 3
+#print(valor)
+
+#operadores ralacionales 
+""" 
+== igual a que 
+!= distinto a que
+> mayor a que 
+< menor a que 
+>= mayor o igual a que
+<= menoy o igual a que 
+"""
+a = '10'
+b = '10'
+
+print(a != b)
+print(a == b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+"""
+ejercicio
+tiempo 5 - 10 minutos
+Si los dos números son iguales
+Si los dos números son diferentes
+Si el primero es mayor que el segundo
+Si el segundo es mayor o igual que el primero
+
+"""
+n1 = int(input("Numero uno: "))
+n2 = int(input("Numero dos: "))
+
+print("los dos numeros son iguales? " + str(n1 == n2))
+print("si los dos numeros son diferentes "+ str(n1 != n2))
+print("el primer numero es mayor que el segundo "+str(n1 > n2))
+print("el segundo numero es mayor o igual que el primero "+ str(n2 >= n1))
+
